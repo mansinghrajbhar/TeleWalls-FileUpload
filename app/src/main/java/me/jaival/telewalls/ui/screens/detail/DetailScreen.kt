2b169f5abe49931876caf08cb84f124fbf98986a
mansinghrajbhar <37462694+mansinghrajbhar@users.noreply.github.com>
2026-09-20 09:48:11 +0530

    Add generic file icon to detail screen

--- app/src/main/java/me/jaival/telewalls/ui/screens/detail/DetailScreen.kt
+++ app/src/main/java/me/jaival/telewalls/ui/screens/detail/DetailScreen.kt
@@ -35,6 +35,7 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Description
 import androidx.compose.material.icons.filled.Download
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Favorite
