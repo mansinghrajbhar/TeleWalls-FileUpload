82096de7ace9908ae67fb9dbab453b58f8f2aafa
mansinghrajbhar <37462694+mansinghrajbhar@users.noreply.github.com>
2026-09-20 12:41:59 +0530

    Fix Compose imports for file UI

--- app/src/main/java/me/jaival/telewalls/ui/screens/collections/CategoryDetailScreen.kt
+++ app/src/main/java/me/jaival/telewalls/ui/screens/collections/CategoryDetailScreen.kt
@@ -1,6 +1,9 @@
 package me.jaival.telewalls.ui.screens.collections
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
